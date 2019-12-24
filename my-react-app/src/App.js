4b5efa90b275cb2e27fa83effff0ae7d9b635e63
Christopher Harding <chrisharding88@gmail.com>
2019-12-24 11:27:41 -0500

    created a function for endGame whenever the game ends

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import nbaCard from './components/Cards'
-import Header from './components/header'
-import Wrapper from './components/wrapper'
+import Card from './components/Cards'
+import Header from './components/Header'
+import Wrapper from './components/Wrapper'
 import cards from './cards.json'
 
 class App extends Component {
-  state = {
+  state={
     current_score: 0,
     high_score: 0,
     winning_score:16,
     cards: []
   };
+
+  endGame = () =>{
+    if (this.state.current_score > this.state.high_score){
+      this.state({high_score: this.state.current_score}, function() {
+        console.log(this.state.high_score)
+      })
+    }
+  }
+
+  clickCount = pic_id => {
+
+  }
+
   render() {
     return (
      <Wrapper>
        <Header current_score={this.state.current_score} high_score={this.state.high_score}></Header>
-       {this.state.cards.map(nbaCard =>( 
-       <Cards>
+       {this.state.cards.map(Card =>( 
+       <Card>
          clickCount={this.clickCount}
          id={cards.pic_id}
          player={cards.player}
          image={cards.image}
-       </Cards>
+       </Card>
        ))}
      </Wrapper>
     );
--- my-react-app/src/components/Cards/cards.js
+++ my-react-app/src/components/Cards/cards.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "/cards.css"
+import "./cards.css"
 
 function Cards (props) {
     console.log(props);
