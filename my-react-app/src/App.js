3084c195c0764843f84363d2a56bb56e155b8b6b
Christopher Harding <chrisharding88@gmail.com>
2019-12-27 12:05:29 -0500

    redo the functions on App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -10,66 +10,62 @@ class App extends Component {
     current_score: 0,
     high_score: 0,
     winning_score: 16,
-    cards: []
+    cards: [],
+    message: ""
   };
 
-  scoreIncrement = () => {
-    this.setState({ current_score: this.state.current_score + 1 })
-  }
+
+  nbaImageClick = (id, playerClicked) => {
+    const cardOrder = this.state.cards;
+
+    if (playerClicked) {
+      cardOrder.forEach((img, i) =>{
+        if (id !== img.id){
+          // If the user clicked the same image
+           cardOrder[i].playerClicked = false
+        }
+      })
+      
+      return this.setState({
+        cards: cardOrder.sort(() => Math.random - 0.5),
+        current_score:0
+      })
 
 
-  endGame = () => {
-    if (this.state.current_score > this.state.high_score) {
-      this.state({ high_score: this.state.current_score }, function () {
-        console.log(this.state.high_score)
+    } else {
+      // If the user clicked the different image
+      cardOrder.forEach((img, i) => {
+          cardOrder[i].playerClicked = true;     
       })
     }
 
-    this.state.cards.forEach(card => {
-      card.count = 0;
+    const scoreIncrement = this.state.current_score + 1
+
+    const newHighScore = this.state.current_score > this.state.high_score ? this.state.current_score : this.state.high_score;
+
+    return this.setState ({
+      cards: cardOrder.sort(() => Math.random() - 0.5),
+      current_score: scoreIncrement,
+      high_score: newHighScore
     });
 
-    // Sets the current score back to zero.
-    this.setState({ current_score: 0 });
-
-    return true;
-
   }
 
-  clickCount = id => {
-    
-    this.state.cards.find((a, b) => {
-
-      if (a.id === id) {
-
-        let scoreCount = cards[b].count;
-
-        if (scoreCount === 0) {
-
-          scoreCount = scoreCount + 1;
-
-          this.scoreIncrement();
-        }
-        this.state.cards.sort(() => Math.random() - 0.5)
-
-        return true;
-
-      } else {
-
-        this.endGame();
-      }
-
-
-
-    })
+  userWins = () =>{
+    if (this.state.current_score === this.state.winning_score){
+      this.setState({message: alert("YOU WIN!!!!")})
+    }
   }
+
+
+
 
   render() {
     return (
       <Wrapper>
         <Header current_score={this.state.current_score} high_score={this.state.high_score}></Header>
         <div>
-        {this.state.cards.map( cards =>
+        {this.state.cards.map( card =>
           <Card
             clickCount={this.clickCount}
             id={cards.pic_id}
